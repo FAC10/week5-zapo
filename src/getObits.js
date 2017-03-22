bc593fc4c102cf31e1b9a589188f990324adb189
oliverjam <oliverjamesphillips@gmail.com>
2017-03-22 23:15:33 +0000

    Beautify and add bracket to fix linter error
    Relates #13

--- src/getObits.js
+++ src/getObits.js
@@ -1,22 +1,26 @@
 require('env2')('.env');
-var createDates = require('./createDates');
+const createDates = require('./createDates');
 
 const request = require('request');
 
-let resultsArr = [];
+const resultsArr = [];
 
 function requests(datesArr, cb) {
-    datesArr.forEach((date => {
-            let url = `https://content.guardianapis.com/search?tag=tone/obituaries&from-date=${date}&order-by=newest&api-key=${process.env.SECRET}&show-fields=trailText`;
-            request(url, (err, res, body) => {
-              let title = res.results[0].webTitle;
-              let url = res.results[0].webUrl;
-              let summary = res.results[0].fields.trailText;
-              const obj = {title, url, summary};
-              resultsArr.push(obj);
-              if(resultsArr.length === datesArr.length) {
-                cb(resultsArr);
-              }
-            });
-        })
-    }
+  datesArr.forEach((date) => {
+    const url = `https://content.guardianapis.com/search?tag=tone/obituaries&from-date=${date}&order-by=newest&api-key=${process.env.SECRET}&show-fields=trailText`;
+    request(url, (err, res, body) => {
+      const title = res.results[0].webTitle;
+      const url = res.results[0].webUrl;
+      const summary = res.results[0].fields.trailText;
+      const obj = {
+        title,
+        url,
+        summary,
+      };
+      resultsArr.push(obj);
+      if (resultsArr.length === datesArr.length) {
+        cb(resultsArr);
+      }
+    });
+  });
+}
