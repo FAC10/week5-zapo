2d45c5bc7f5bdd5ac1a2cefca07458d099a7be9b
Zooey <zooey@lbk.org.uk>
2017-03-22 22:08:57 +0000

    add tests for createDates I have added tests for the createDates object and we now have 100% coverage, I have also changed the format of our tests slightly, rather than our test script running all of the different test files individually, it instead runs on test.js which requires in all of the other tests. This seems a little bit cleaner, though perhaps more prone to user error. Happy to go back to the other way if preferred. relates to #3

--- src/createDates.js
+++ src/createDates.js
@@ -20,16 +20,19 @@
 //   }
 // }
 
-// date object -> date string
+// date object instance -> date string
 
 function createDate(date) {
   return date.toISOString().slice(0, 10);
 }
 
+// ISODate string -> array of 10 dates
+
 function createDateArr(ISOdate) {
   const arr = [];
   while (arr.length < 11) {
-    arr.push(Number(ISOdate.substring(0, 4)) - arr.length);
+    const newYear = Number(ISOdate.substring(0, 4)) - arr.length;
+    arr.push(newYear + ISOdate.substring(4, ISOdate.length));
   }
   return arr;
 }
@@ -39,6 +42,8 @@ const makeDates = {
   createDate,
   createDateArr,
 };
+
+// console.log(makeDates.createDateArr('1993-02-03'));
 
 
 module.exports = makeDates;
