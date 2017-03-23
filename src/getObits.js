9c80135d5cd957560fb6faa9cdf23f3ab6d2e9ad
oliverjam <oliverjamesphillips@gmail.com>
2017-03-23 00:20:39 +0000

    Use todate rather than fromdate in API URL
    fromdate ends up giving us only the newest obit, rather than the newest on the date we specify
    
    Relates #13

--- src/getObits.js
+++ src/getObits.js
@@ -6,7 +6,7 @@ const request = require('request');
 function getObits(datesArr, cb) {
   const resultsArr = [];
   datesArr.forEach((date) => {
-    const url = `https://content.guardianapis.com/search?tag=tone/obituaries&from-date=${date}&order-by=newest&api-key=${process.env.SECRET}&show-fields=trailText`;
+    const url = `https://content.guardianapis.com/search?tag=tone/obituaries&to-date=${date}&order-by=newest&api-key=${process.env.SECRET}&show-fields=trailText`;
     request(url, (err, res, body) => {
       // I thought Request provided JSON automatically but apparently not
       const data = JSON.parse(body).response;
