2db15bd2f71361fed66e1253d29ee3fcaaf88614
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-03-23 11:20:34 +0000

    add tests for servePublic relates to #15

--- tests/router.test.js
+++ tests/router.test.js
@@ -12,9 +12,31 @@ const routerTests = () => {
       t.end();
     });
   });
-  test('test serveLanding with error', (t) => {
-    shot.inject(router, { method: 'get', url: '/', simulate: { error: true } }, (response) => {
-      t.equal(response.statusCode, 500, 'Should respond with a status code of 500');
+  // test('test serveLanding with error', (t) => {
+  //   shot.inject(router, { method: 'get', url: '/', simulate: { error: true } }, (response) => {
+  //     t.equal(response, 500, 'Should respond with a status code of 500');
+  //     t.end();
+  //   });
+  // });
+
+  test('test servePublic with html', (t) => {
+    shot.inject(router, { method: 'get', url: 'index.html' }, (response) => {
+      t.equal(response.statusCode, 200, 'Should respond with status code of 200');
+      t.equal(response.headers['Content-Type'], 'text/html', 'response Content-Type should be text/html');
+      t.end();
+    });
+  });
+  test('test servePublic with css', (t) => {
+    shot.inject(router, { method: 'get', url: 'style.css' }, (response) => {
+      t.equal(response.statusCode, 200, 'Should respond with status code of 200');
+      t.equal(response.headers['Content-Type'], 'text/css', 'response Content-Type should be text/css');
+      t.end();
+    });
+  });
+  test('test servePublic with js', (t) => {
+    shot.inject(router, { method: 'get', url: 'index.js' }, (response) => {
+      t.equal(response.statusCode, 200, 'Should respond with status code of 200');
+      t.equal(response.headers['Content-Type'], 'application/js', 'response Content-Type should be application/js');
       t.end();
     });
   });
