329ee69625327a9d35a43e3997ad16f2859e7a94
pbywater <philippabywater@gmail.com>
2017-03-23 16:02:32 +0000

    serving svg files now works
    relates to #26

--- src/routers/router.js
+++ src/routers/router.js
@@ -5,7 +5,7 @@ module.exports = function (request, response) {
   const extension = url.split('.')[1];
   if (url === '/') {
     handlers.serveLanding(request, response);
-  } else if (extension === 'css' || extension === 'js' || extension === 'html' || extension === 'ico' || extension === 'png') {
+  } else if (extension === 'css' || extension === 'js' || extension === 'html' || extension === 'ico' || extension === 'png' || extension === 'svg') {
     handlers.servePublic(request, response, url);
   } else if (url.indexOf('/api') !== -1) { // TBC
     handlers.serveAPI(request, response);
