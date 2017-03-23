884c5527d005c13770e34cdf576ffdcc0178d1ab
oliverjam <oliverjamesphillips@gmail.com>
2017-03-23 12:35:34 +0000

    Write frontend DOM updating
    Relates #22

---
+++ public/index.js
@@ -0,0 +1,83 @@
+/* eslint-disable */
+var fakeData = [
+    {
+        title: 'Diana Wallace obituary',
+        url: 'https://www.theguardian.com/education/2017/mar/22/diana-wallace-obituary',
+        summary: '<strong>Other lives: </strong>Social worker who was guided by her Christian faith'
+    }, {
+        title: 'Diana Wallace obituary',
+        url: 'https://www.theguardian.com/education/2017/mar/22/diana-wallace-obituary',
+        summary: '<strong>Other lives: </strong>Social worker who was guided by her Christian faith'
+    }, {
+        title: 'Diana Wallace obituary',
+        url: 'https://www.theguardian.com/education/2017/mar/22/diana-wallace-obituary',
+        summary: '<strong>Other lives: </strong>Social worker who was guided by her Christian faith'
+    }, {
+        title: 'Diana Wallace obituary',
+        url: 'https://www.theguardian.com/education/2017/mar/22/diana-wallace-obituary',
+        summary: '<strong>Other lives: </strong>Social worker who was guided by her Christian faith'
+    }, {
+        title: 'Diana Wallace obituary',
+        url: 'https://www.theguardian.com/education/2017/mar/22/diana-wallace-obituary',
+        summary: '<strong>Other lives: </strong>Social worker who was guided by her Christian faith'
+    }, {
+        title: 'Diana Wallace obituary',
+        url: 'https://www.theguardian.com/education/2017/mar/22/diana-wallace-obituary',
+        summary: '<strong>Other lives: </strong>Social worker who was guided by her Christian faith'
+    }, {
+        title: 'Diana Wallace obituary',
+        url: 'https://www.theguardian.com/education/2017/mar/22/diana-wallace-obituary',
+        summary: '<strong>Other lives: </strong>Social worker who was guided by her Christian faith'
+    }, {
+        title: 'Diana Wallace obituary',
+        url: 'https://www.theguardian.com/education/2017/mar/22/diana-wallace-obituary',
+        summary: '<strong>Other lives: </strong>Social worker who was guided by her Christian faith'
+    }, {
+        title: 'Diana Wallace obituary',
+        url: 'https://www.theguardian.com/education/2017/mar/22/diana-wallace-obituary',
+        summary: '<strong>Other lives: </strong>Social worker who was guided by her Christian faith'
+    }, {
+        title: 'Diana Wallace obituary',
+        url: 'https://www.theguardian.com/education/2017/mar/22/diana-wallace-obituary',
+        summary: '<strong>Other lives: </strong>Social worker who was guided by her Christian faith'
+    }
+];
+
+function updateDOM(dataArr) {
+  dataArr.forEach(function(obit) {
+    var container = createEl('div', 'obit', null, null);
+    var link = createEl('a', 'obit__link', null, obit.url);
+    var title = createEl('h2', 'obit__title', obit.title);
+    var summary = createEl('p', 'obit__summary', obit.summary);
+    link.appendChild(title);
+    container.appendChild(link);
+    container.appendChild(summary);
+    var app = document.getElementById('app');
+    app.appendChild(container);
+  })
+}
+
+updateDOM(fakeData);
+
+function createEl(element, className, text, url) {
+  var el = document.createElement(element);
+  el.className = className || '';
+  el.textContent = text || '';
+  if (url) el.href = url;
+  return el;
+}
+
+function fetch(method, url, responseCallback) {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (request.readyState === 4 && request.status === 200) {
+            var data = JSON.parse(request.responseText);
+            responseCallback(null, data);
+        }
+    };
+    request.onerror = function() {
+        responseCallback('Sorry, connection error');
+    }
+    request.open(method, url, true);
+    request.send();
+}
